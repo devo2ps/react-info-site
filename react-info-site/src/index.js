6bbac85caed587c64464460b405eb62e5a9eaa91
devo2ps <50072147+devo2ps@users.noreply.github.com>
2023-07-04 00:00:11 -0500

    added ul elts

--- react-info-site/src/index.js
+++ react-info-site/src/index.js
@@ -2,4 +2,5 @@
 // Re-write the line of code as best you can from memory
 import ReactDOM from 'react-dom'
 import React from 'react'
-ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+// ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root")
+ReactDOM.render(<ul><li>Thing 1</li><li>Thing 2</li></ul>, document.getElementById("root"))
